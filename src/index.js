2a73792929cdb513cb5e7f5142cce44739e5a2e7
Aili Priimann <ailipriimann@Ailis-MacBook-Pro.local>
2021-01-21 13:05:19 +0200

    better algorithm

--- src/index.js
+++ src/index.js
@@ -1,29 +1,29 @@
-const minValue = 100;
-const maxValue = 200;
-const maxGuesses= 15;
+const minValue = 0;
+const maxValue = 100;
+const maxGuesses = 15;
 
-const minValueText = document.querySelector('.minValue');
-minValueText.innerHTML = ' ' + minValue + ' ';
-const maxValueText = document.querySelector('.maxValue');
-maxValueText.innerHTML = ' ' + maxValue;
-const maxGuessesText = document.querySelector('.maxGuesses');
-maxGuessesText.innerHTML = ' ' + maxGuesses + ' ';
-const btnRobotPlay = document.querySelector('.robot-play');
+const minValueText = document.querySelector(".minValue");
+minValueText.innerHTML = " " + minValue + " ";
+const maxValueText = document.querySelector(".maxValue");
+maxValueText.innerHTML = " " + maxValue;
+const maxGuessesText = document.querySelector(".maxGuesses");
+maxGuessesText.innerHTML = " " + maxGuesses + " ";
+const btnRobotPlay = document.querySelector(".robot-play");
 
 // ******* counter and total guesses *******
-const totalGuessesText = document.querySelector('.totalGuesses');
-const counterText = document.querySelector('.counter');
-let counterStart = '';
-let counterEnd = '';
+const totalGuessesText = document.querySelector(".totalGuesses");
+const counterText = document.querySelector(".counter");
+let counterStart = "";
+let counterEnd = "";
 
 let randomNumber = Math.floor(Math.random() * (maxValue - minValue) + minValue);
 
-const guesses = document.querySelector('.guesses');
-const lastResult = document.querySelector('.lastResult');
-const lowOrHi = document.querySelector('.lowOrHi');
+const guesses = document.querySelector(".guesses");
+const lastResult = document.querySelector(".lastResult");
+const lowOrHi = document.querySelector(".lowOrHi");
 
-const guessSubmit = document.querySelector('.guessSubmit');
-const guessField = document.querySelector('.guessField');
+const guessSubmit = document.querySelector(".guessSubmit");
+const guessField = document.querySelector(".guessField");
 
 let guessCount = 1;
 let resetButton;
@@ -33,52 +33,52 @@ guessField.focus();
 const checkGuess = () => {
   let userGuess = Number(guessField.value);
   if (guessCount === 1) {
-    guesses.textContent = 'Previous guesses: ';
+    guesses.textContent = "Previous guesses: ";
     counterStart = Date.now();
   }
-  guesses.textContent += userGuess + ' ';
+  guesses.textContent += userGuess + " ";
 
   if (userGuess === randomNumber) {
     counterEnd = Date.now();
-    lastResult.textContent = 'Congratulations! You got it right!';
-    lastResult.style.backgroundColor = 'green';
-    lowOrHi.textContent = '';
-    lastResult.style.padding = '10px';
-    lastResult.style.textAlign = 'center';
+    lastResult.textContent = "Congratulations! You got it right!";
+    lastResult.style.backgroundColor = "green";
+    lowOrHi.textContent = "";
+    lastResult.style.padding = "10px";
+    lastResult.style.textAlign = "center";
     let counter = Math.floor((counterEnd - counterStart) / 1000);
-    counterText.innerHTML = 'counter: ' + counter + ' seconds';
-    totalGuessesText.innerHTML = 'total guesses: ' + guessCount;
+    counterText.innerHTML = "counter: " + counter + " seconds";
+    totalGuessesText.innerHTML = "total guesses: " + guessCount;
     setGameOver();
   } else if (guessCount === maxGuesses) {
-    lastResult.textContent = '!!!GAME OVER!!!';
+    lastResult.textContent = "!!!GAME OVER!!!";
     setGameOver();
   } else {
-    lastResult.textContent = 'Wrong!';
-    lastResult.style.backgroundColor = 'red';
-    lastResult.style.padding = '10px';
-    lastResult.style.textAlign = 'center';
-    if(userGuess < randomNumber) {
-      lowOrHi.textContent = 'Last guess was too low!';
-    } else if(userGuess > randomNumber) {
-      lowOrHi.textContent = 'Last guess was too high!';
+    lastResult.textContent = "Wrong!";
+    lastResult.style.backgroundColor = "red";
+    lastResult.style.padding = "10px";
+    lastResult.style.textAlign = "center";
+    if (userGuess < randomNumber) {
+      lowOrHi.textContent = "Last guess was too low!";
+    } else if (userGuess > randomNumber) {
+      lowOrHi.textContent = "Last guess was too high!";
     }
   }
 
   guessCount++;
-  guessField.value = '';
+  guessField.value = "";
   guessField.focus();
 };
 
-guessSubmit.addEventListener('click', checkGuess);
+guessSubmit.addEventListener("click", checkGuess);
 
 const setGameOver = () => {
   guessField.disabled = true;
   guessSubmit.disabled = true;
-  resetButton = document.createElement('button');
-  resetButton.textContent = 'Start new game';
-  resetButton.classList.add('play-again');
+  resetButton = document.createElement("button");
+  resetButton.textContent = "Start new game";
+  resetButton.classList.add("play-again");
   document.body.append(resetButton);
-  resetButton.addEventListener('click', resetGame);
+  resetButton.addEventListener("click", resetGame);
 };
 
 const resetGame = () => {
@@ -87,40 +87,36 @@ const resetGame = () => {
   newMax = 0;
   newMin = 0;
 
-  const resetParas = document.querySelectorAll('.resultParas p');
-  for (let i = 0 ; i < resetParas.length ; i++) {
-    resetParas[i].textContent = '';
+  const resetParas = document.querySelectorAll(".resultParas p");
+  for (let i = 0; i < resetParas.length; i++) {
+    resetParas[i].textContent = "";
   }
 
   resetButton.parentNode.removeChild(resetButton);
 
   guessField.disabled = false;
   guessSubmit.disabled = false;
-  guessField.value = '';
+  guessField.value = "";
   guessField.focus();
 
-  lastResult.style.backgroundColor = 'white';
+  lastResult.style.backgroundColor = "white";
 
   randomNumber = Math.floor(Math.random() * (maxValue - minValue) + minValue);
 };
 
-let robotAlgorithm = () => {
-  centerValue = maxValue - Math.floor(maxValue - oldCenterValue / 2);
-
-};
 //robot algorithm
 let robotQuess = maxValue - Math.floor((maxValue - minValue) / 2);
-let newMax = 0;
-let newMin = 0;
+let newMax = maxValue;
+let newMin = minValue;
 console.log(robotQuess);
 
 const robotPlay = () => {
-if (guessCount === 1) {
-    guesses.textContent = 'Previous guesses: ';
+  if (guessCount === 1) {
+    guesses.textContent = "Previous guesses: ";
   }
 
   while (guessCount <= maxGuesses) {
-    guesses.textContent += robotQuess + ' ';
+    guesses.textContent += robotQuess + " ";
     if (robotQuess === randomNumber) {
       setGameOver();
       break;
@@ -129,45 +125,128 @@ if (guessCount === 1) {
       break;
     } else {
       if (robotQuess > randomNumber) {
-        if (newMax == 0) {
+        if (newMax == 0 && newMin == 0) {
           newMax = robotQuess;
           robotQuess -= Math.floor((maxValue - robotQuess) / 2);
-          console.log('random: ' + randomNumber);
+          console.log("random: " + randomNumber);
           console.log(robotQuess);
         } else if (newMin !== 0) {
-            newMax = robotQuess;
-            robotQuess -= Math.floor((robotQuess - newMin) / 2);
-            console.log('random: ' + randomNumber);
-            console.log(robotQuess);
+          newMax = robotQuess;
+          robotQuess -= Math.floor((robotQuess - newMin) / 2);
+          console.log("random: " + randomNumber);
+          console.log(robotQuess);
         } else {
           newMax = robotQuess;
-          robotQuess -= Math.floor((robotQuess - minValue) / 2);
-          console.log('random: ' + randomNumber);
+          robotQuess -= Math.ceil((robotQuess - minValue) / 2);
+          console.log("random: " + randomNumber);
           console.log(robotQuess);
         }
-      } else if (robotQuess < randomNumber){
-        if (newMin == 0) {
+      } else if (robotQuess < randomNumber) {
+        if (newMin == 0 && newMax == 0) {
           newMin = robotQuess;
-          robotQuess += Math.ceil((maxValue - robotQuess) / 2);
-          console.log('random: ' + randomNumber);
+          robotQuess += Math.floor((maxValue - robotQuess) / 2);
+          console.log("random: " + randomNumber);
           console.log(robotQuess);
         } else if (newMax == 0) {
-          robotQuess += Math.ceil((newMin + robotQuess) / 2);
-          console.log('random: ' + randomNumber);
+          newMin = robotQuess;
+          robotQuess += Math.floor((maxValue - newMin) / 2);
+          console.log("random: " + randomNumber);
           console.log(robotQuess);
         } else {
           newMin = robotQuess;
-          robotQuess += Math.ceil((newMax - robotQuess) / 2);
-          console.log('random: ' + randomNumber);
+          robotQuess += Math.floor((newMax - robotQuess) / 2);
+          console.log("random: " + randomNumber);
           console.log(robotQuess);
         }
       }
       guessCount++;
     }
   }
+  totalGuessesText.innerHTML = "total guesses: " + guessCount;
 };
 
 //let robot play
-btnRobotPlay.addEventListener('click', () => {
+btnRobotPlay.addEventListener("click", () => {
   robotPlay();
 });
+
+
+//robot algorithm
+let robotAlgorithm = () => {
+  guessCount = 0;
+  let robotArray = [];
+  let robotQuess = maxValue - Math.floor((maxValue - minValue) / 2);
+  let newMax = maxValue;
+  let newMin = minValue;
+  console.log(robotQuess);
+  let wins = 0;
+  let losses = 0;
+  let totalQuessMin = 0;
+  let totalQuessMax = 0;
+  let totalQuessAverage = 0;
+
+  for (let i = 0; i < 100; i++) {
+    randomNumber = Math.floor(Math.random() * (maxValue - minValue) + minValue);
+    if (guessCount === 1) {
+      console.log("Previous guesses: ");
+    }
+
+    while (guessCount <= maxGuesses) {
+      robotArray.push(robotQuess);
+
+      if (robotQuess === randomNumber) {
+        console.log("You won!");
+        wins++;
+        break;
+      } else if (guessCount === maxGuesses) {
+        console.log("Game Over!");
+        losses++;
+        break;
+      } else {
+        if (robotQuess > randomNumber) {
+          if (newMax == 0 && newMin == 0) {
+            newMax = robotQuess;
+            robotQuess -= Math.floor((maxValue - robotQuess) / 2);
+          } else if (newMin !== 0) {
+            newMax = robotQuess;
+            robotQuess -= Math.floor((robotQuess - newMin) / 2);
+          } else {
+            newMax = robotQuess;
+            robotQuess -= Math.ceil((robotQuess - minValue) / 2);
+          }
+        } else if (robotQuess < randomNumber) {
+          if (newMin == 0 && newMax == 0) {
+            newMin = robotQuess;
+            robotQuess += Math.floor((maxValue - robotQuess) / 2);
+          } else if (newMax == 0) {
+            newMin = robotQuess;
+            robotQuess += Math.floor((maxValue - newMin) / 2);
+          } else {
+            newMin = robotQuess;
+            robotQuess += Math.floor((newMax - robotQuess) / 2);
+          }
+        }
+        guessCount++;
+      }
+    }
+    for (quess of robotArray) {
+      console.log(quess);
+    }
+    if (totalQuessMin == 0 && totalQuessMax == 0) {
+      totalQuessMin = guessCount;
+      totalQuessMax = guessCount;
+    } else if (totalQuessMax < guessCount) {
+      totalQuessMax = guessCount;
+    } else if (totalQuessMin > guessCount) {
+      totalQuessMin = guessCount;
+    }
+    console.log(randomNumber);
+    console.log('min total guess count: ' + totalQuessMin);
+    console.log('max total guess count: ' + totalQuessMax);
+  }
+  console.log('Total wins:' + wins);
+  console.log('Total losses:' + losses);
+};
+
+console.log(robotAlgorithm());
+
