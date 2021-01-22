8643ab7d4073c6c997d554c40da3639302e8d5ae
Aili Priimann <ailipriimann@Ailis-MacBook-Pro.local>
2021-01-22 11:16:15 +0200

    better code

--- src/index.js
+++ src/index.js
@@ -6,28 +6,6 @@ const data = [
   { name: "Pureed root vegetable soup with smoked cheese", price: 8.0 },
 ];
 
-//create buttons
-const menu = document.querySelector(".small-menu");
-const btnVal = document.createElement("button");
-btnVal.textContent = "validate";
-menu.appendChild(btnVal);
-
-const btnSort = document.createElement("button");
-btnSort.textContent = "sort";
-menu.appendChild(btnSort);
-
-const btnFilter = document.createElement("button");
-btnFilter.textContent = "filter";
-menu.appendChild(btnFilter);
-
-const btnMap = document.createElement("button");
-btnMap.textContent = "map";
-menu.appendChild(btnMap);
-
-const btnReduce = document.createElement("button");
-btnReduce.textContent = "Reduce";
-menu.appendChild(btnReduce);
-
 /**
  * 1.Function validate meal name based on regex rules
  *
@@ -50,11 +28,11 @@ for (const item of Object.values(data)) {
  *
  * @param {Array} menu
  */
-const sortMenu2 = menu => {
+const sortMenu = menu => {
   const sortedMenu = menu.sort((a, b) => a.price - b.price);
   return sortedMenu;
 };
-console.log('sorted menu', sortMenu2(data));
+console.log('sorted menu', sortMenu(data));
 
 
 /**
@@ -124,20 +102,6 @@ const selectVegeMeals = (menuData) => {
 console.log('test vege meal function', selectVegeMeals(FazerMenu));
 
 
-//validate the name
-const validateName = menu => {
-  const regexpPattern = new RegExp('/^[A-ZÖÄÅ]{1}[a-zöäå][0-9][" ", ",", "-", "/", "(", ")"]{4,64}$/m');
-  let res = true;
-  for (const item of menu) {
-    if (!regexpPattern.test(item)) {
-      console.log(`Wont Match '${regexpPattern}'`);
-      res = false;
-    }
-    return res;
-  };
-};
-console.log(validateName(data));
-
 //render a menu
 const renderMenu = menu => {
   const list = document.querySelector(".restorant");
@@ -149,7 +113,7 @@ const renderMenu = menu => {
   }
 };
 
-//Always first sort by name
+/*sort by name
 data.sort(function (a, b) {
   var nameA = a.name.toUpperCase(); // ignore upper and lowercase
   var nameB = b.name.toUpperCase(); // ignore upper and lowercase
@@ -161,63 +125,4 @@ data.sort(function (a, b) {
   }
   // names must be equal
   return 0;
-});
-
-//sort by value when pressed button
-const sortMenu = (menu) => {
-  menu.sort((a, b) => {
-    return a.price - b.price;
-  });
-  renderMenu(data);
-};
-
-//filter
-function filteredByprice(item) {
-  if (item.price < 5) {
-    return item;
-  }
-}
-
-let filterMenu = (menu) => {
-  let ArrByPrice = menu.filter(filteredByprice);
-  renderMenu(ArrByPrice);
-};
-
-//map
-const mapByPrice = item => {
-  let newPrice = item * 1.15;
-  let rounded = Number(Math.round(newPrice+'e'+2)+'e-'+2);
-    return rounded;
-  };
-
-const mapMenu = data.map(item => {
-  const container = {};
-  container.name = item.name;
-  container.price = mapByPrice(item.price);
-  return container;
-});
-
-//Reduce
-const reduceMenu = data.reduce(function (acc, obj) { return acc + obj.price; }, 0);
-console.log(reduceMenu);
-
-//init
-const init = () => {
-  btnVal.addEventListener("click", () => {
-    window.alert(validateName(data));
-  });
-  btnSort.addEventListener("click", () => {
-    sortMenu(data);
-  });
-  btnFilter.addEventListener("click", () => {
-    filterMenu(data);
-  });
-  btnMap.addEventListener("click", () => {
-    renderMenu(mapMenu);
-  });
-  btnReduce.addEventListener("click", () => {
-    window.alert(reduceMenu);
-  });
-  renderMenu(data);
-};
-init();
+});*/
